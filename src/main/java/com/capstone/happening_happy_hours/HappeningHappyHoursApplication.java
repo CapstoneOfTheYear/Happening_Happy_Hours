33987af046f238b8dceb5cac4630bf5eb6bc751e
Charles Willcockson <willcockson.charles@gmail.com>
2022-04-29 17:14:08 -0500

    first push with file structure

---
+++ src/main/java/com/capstone/happening_happy_hours/HappeningHappyHoursApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.happening_happy_hours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappeningHappyHoursApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HappeningHappyHoursApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/capstone/happening_happy_hours/HappeningHappyHoursApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.happening_happy_hours;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappeningHappyHoursApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
