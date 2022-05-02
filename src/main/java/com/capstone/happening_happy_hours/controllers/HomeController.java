61b3a995425fe2737416f6a14dc4f94adf8bcf03
Charles Willcockson <willcockson.charles@gmail.com>
2022-05-01 21:31:40 -0500

    added home contorller

---
+++ src/main/java/com/capstone/happening_happy_hours/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package com.capstone.happening_happy_hours.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String landing() {
+        return "This is the landing page for Happening Happy Hours!";
+    }
+}
