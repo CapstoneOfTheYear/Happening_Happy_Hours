919add4300d7f1232cada170f1202f5c4fbaab51
Jonathan Clark <jonathan.b.clark1127@gmail.com>
2022-05-03 12:31:06 -0500

    working on login and register forms

--- src/main/java/com/capstone/happening_happy_hours/controllers/UserController.java
+++ src/main/java/com/capstone/happening_happy_hours/controllers/UserController.java
@@ -1,12 +1,25 @@
 package com.capstone.happening_happy_hours.controllers;
 
 
+import com.capstone.happening_happy_hours.models.User;
+import com.capstone.happening_happy_hours.repositories.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UserController {
+    private UserRepository userDao;
+
+    public UserController(UserRepository userDao) {
+        this.userDao = userDao;
+    }
+
+
     @GetMapping("/profile/user")
 //    @ResponseBody
 //    public String user() {
@@ -17,7 +30,14 @@ public class UserController {
     }
 
     @GetMapping("/register")
-    public String register() {
+    public String register(Model model) {
+        model.addAttribute("user", new User());
         return "register";
+    }
+
+    @PostMapping("/register")
+    public String saveUser(@ModelAttribute User user){
+        userDao.save(user);
+        return "redirect:/login";
     }
 }
