a0b6d9584810efcde8e8753531bef424c384d8d9
Charles Willcockson <willcockson.charles@gmail.com>
2022-05-04 11:08:24 -0500

    finished businessAttribute model

--- src/main/java/com/capstone/happening_happy_hours/models/BusinessAttribute.java
+++ src/main/java/com/capstone/happening_happy_hours/models/BusinessAttribute.java
@@ -35,4 +35,93 @@ public class BusinessAttribute {
     @Column(nullable = false)
     private String otherGames;
 
+    public BusinessAttribute(){
+
+    }
+
+    public BusinessAttribute(long id, boolean dogsAllowed, boolean pool, boolean darts, boolean liveMusic, boolean karaoke, boolean cornhole, boolean servesFood, String otherGames) {
+        this.id = id;
+        this.dogsAllowed = dogsAllowed;
+        this.pool = pool;
+        this.darts = darts;
+        this.liveMusic = liveMusic;
+        this.karaoke = karaoke;
+        this.cornhole = cornhole;
+        this.servesFood = servesFood;
+        this.otherGames = otherGames;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public boolean isDogsAllowed() {
+        return dogsAllowed;
+    }
+
+    public void setDogsAllowed(boolean dogsAllowed) {
+        this.dogsAllowed = dogsAllowed;
+    }
+
+    public boolean isPool() {
+        return pool;
+    }
+
+    public void setPool(boolean pool) {
+        this.pool = pool;
+    }
+
+    public boolean isDarts() {
+        return darts;
+    }
+
+    public void setDarts(boolean darts) {
+        this.darts = darts;
+    }
+
+    public boolean isLiveMusic() {
+        return liveMusic;
+    }
+
+    public void setLiveMusic(boolean liveMusic) {
+        this.liveMusic = liveMusic;
+    }
+
+    public boolean isKaraoke() {
+        return karaoke;
+    }
+
+    public void setKaraoke(boolean karaoke) {
+        this.karaoke = karaoke;
+    }
+
+    public boolean isCornhole() {
+        return cornhole;
+    }
+
+    public void setCornhole(boolean cornhole) {
+        this.cornhole = cornhole;
+    }
+
+    public boolean isServesFood() {
+        return servesFood;
+    }
+
+    public void setServesFood(boolean servesFood) {
+        this.servesFood = servesFood;
+    }
+
+    public String getOtherGames() {
+        return otherGames;
+    }
+
+    public void setOtherGames(String otherGames) {
+        this.otherGames = otherGames;
+    }
 }
+
+
