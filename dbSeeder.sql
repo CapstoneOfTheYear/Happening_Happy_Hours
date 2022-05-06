d4373596b32e4844ccb5ae62f61b9b7bfb815e3d
Robert Young <94646951+RobertYoung2022@users.noreply.github.com>
2022-05-06 13:54:46 -0500

    added new mapbox feature

--- dbSeeder.sql
+++ dbSeeder.sql
@@ -1,9 +1,9 @@
 USE h3_db;
 
-INSERT INTO h3_db.businesses (city, location, name, postal_code, state, starting_hour, ending_hour)
-values('San Antonio', '2720 McCullough Ave', 'Barbaro', '78212', 'TX', '4', '6'),
-      ('San Antonio', '155 E Commerce St', 'The Esquire Tavern', '78205', 'TX', '3', '7'),
-      ('San Antonio', '5800 Broadway #300', 'Paloma Blanca Mexican Cuisine', '78209', 'TX', '4', '7');
+INSERT INTO h3_db.businesses (id, city, location, name, postal_code, state, starting_hour, ending_hour)
+values(1,'San Antonio', '2720 McCullough Ave', 'Barbaro', '78212', 'TX', '4', '6'),
+      (2, 'San Antonio', '155 E Commerce St', 'The Esquire Tavern', '78205', 'TX', '3', '7'),
+      (3, 'San Antonio', '5800 Broadway', 'Paloma Blanca Mexican Cuisine', '78209', 'TX', '4', '7');
 
 insert into h3_db.user (email, owns_business, password, username)
 values ('yourMom@hoeSpace.org', 0, 'codeup', 'Foodie210'),
