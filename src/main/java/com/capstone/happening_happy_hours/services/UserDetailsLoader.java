539b2b1f685e42364139511778c1b79173529569
Jonathan Clark <jonathan.b.clark1127@gmail.com>
2022-05-06 14:29:06 -0500

    fixed build issues

--- src/main/java/com/capstone/happening_happy_hours/services/UserDetailsLoader.java
+++ src/main/java/com/capstone/happening_happy_hours/services/UserDetailsLoader.java
@@ -1,26 +1,26 @@
-//package com.capstone.happening_happy_hours.services;
-//import com.capstone.happening_happy_hours.models.User;
-//import com.capstone.happening_happy_hours.models.UserWithRoles;
-//import com.capstone.happening_happy_hours.repositories.UserRepository;
-//import org.springframework.stereotype.Service;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.core.userdetails.UsernameNotFoundException;
-//
-//@Service
-//public class UserDetailsLoader implements UserDetailsService {
-//    private final UserRepository users;
-//
-//    public UserDetailsLoader(UserRepository users) {
-//        this.users = users;
-//    }
-//    @Override
-//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//        User user = users.findByUsername(username);
-//        if (user == null) {
-//            throw new UsernameNotFoundException("No user found for " + username);
-//        }
-//
-//        return new UserWithRoles(user);
-//    }
-//}
+package com.capstone.happening_happy_hours.services;
+import com.capstone.happening_happy_hours.models.User;
+import com.capstone.happening_happy_hours.models.UserWithRoles;
+import com.capstone.happening_happy_hours.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Service
+public class UserDetailsLoader implements UserDetailsService {
+    private final UserRepository users;
+
+    public UserDetailsLoader(UserRepository users) {
+        this.users = users;
+    }
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = users.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("No user found for " + username);
+        }
+
+        return new UserWithRoles(user);
+    }
+}
