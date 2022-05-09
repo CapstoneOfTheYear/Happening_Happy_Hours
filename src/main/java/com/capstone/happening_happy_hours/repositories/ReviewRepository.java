76401dda52ed7d14f7ed0b08a8cf38ac04967d12
Charles Willcockson <willcockson.charles@gmail.com>
2022-05-09 16:56:13 -0500

    changes to user profiule page

--- src/main/java/com/capstone/happening_happy_hours/repositories/ReviewRepository.java
+++ src/main/java/com/capstone/happening_happy_hours/repositories/ReviewRepository.java
@@ -3,7 +3,10 @@ package com.capstone.happening_happy_hours.repositories;
 import com.capstone.happening_happy_hours.models.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     Review findReviewsById(long id);
+    List<Review> findAllByUserId(long id);
 }
