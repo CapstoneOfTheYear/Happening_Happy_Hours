e8fb18ad935e699c0242691860058217cb465d4c
Charles Willcockson <willcockson.charles@gmail.com>
2022-05-09 15:10:39 -0500

    changed business repo to have get by Id

--- src/main/java/com/capstone/happening_happy_hours/repositories/BusinessRepository.java
+++ src/main/java/com/capstone/happening_happy_hours/repositories/BusinessRepository.java
@@ -4,5 +4,6 @@ import com.capstone.happening_happy_hours.models.Business;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BusinessRepository extends JpaRepository <Business, Long> {
-
+    Business getBusinessById(long id);
+    Business getBusinessByName(String name);
 }
