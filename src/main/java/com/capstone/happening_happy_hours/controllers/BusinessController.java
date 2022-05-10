a7f4dea42f303140abe6302f092f43b59ef61e22
Jonathan Clark <jonathan.b.clark1127@gmail.com>
2022-05-10 13:35:35 -0500

    working through edit

--- src/main/java/com/capstone/happening_happy_hours/controllers/BusinessController.java
+++ src/main/java/com/capstone/happening_happy_hours/controllers/BusinessController.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -39,9 +40,32 @@ public class BusinessController {
         return "businessProfile";
     }
 
+    @GetMapping("/updateBusiness")
+    public String updateProfile(Model model){
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Business business = businessDao.getBusinessByUser(user);
+        System.out.println("business.getName() = " + business.getName());
+        model.addAttribute("user", user);
+        model.addAttribute("business", business);
+        return "updateBusiness";
+   }
+
+
+    @PostMapping("/updateBusiness")
+    public String postUpdateProfile(Model model){
+
+       return "redirect: businessProfile";
+    }
+
+
+
+
     @GetMapping("/business")
     public String singleBusinessProfile(Model model) {
 
         return "viewBusiness";
     }
+
+
+
 }
