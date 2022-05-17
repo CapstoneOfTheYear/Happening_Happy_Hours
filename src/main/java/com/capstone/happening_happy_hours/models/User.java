fbc9e4a8a63dd40c75d44d3bb2261dc2bfff8754
Charles Willcockson <willcockson.charles@gmail.com>
2022-05-17 12:07:40 -0500

    made username unique

--- src/main/java/com/capstone/happening_happy_hours/models/User.java
+++ src/main/java/com/capstone/happening_happy_hours/models/User.java
@@ -15,7 +15,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @Column(nullable = false, length = 100)
+    @Column(nullable = false, unique = true, length = 100)
     private String username;
 
     @Column
@@ -24,7 +24,7 @@ public class User {
     @Column(nullable = false, length = 100)
     private String email;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private String password;
 
     @Column
